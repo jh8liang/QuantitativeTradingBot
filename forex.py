663ac85e99b0bd878dfcb628bdc7914caa43b2d8
ohshizournizz <jamiejiahao@gmail.com>
2019-01-08 18:33:24 -0500

    setting up the project

---
+++ forex.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+base_url = 'http://forex.1forge.com/1.0.3/'
+
+def main():
+    requests.get('http://forex.1forge.com/1.0.3/')
+    print('hello')
+
+
+if __name__ == '__main__':
+	main()
