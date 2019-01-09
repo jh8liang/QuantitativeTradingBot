1ec09718c8f54feae3c1f6b56457a25112ba7a94
ohshizournizz <jamiejiahao@gmail.com>
2019-01-09 09:37:24 -0500

    minor changes

--- forex.py
+++ forex.py
@@ -5,7 +5,7 @@ ApiKey = 'lPpKLU2BhzWU3eB2iWHeZ0XG5fO241JP'
 base_url = 'http://forex.1forge.com/1.0.3/'
 
 def main():
-    response = requests.get(base_url + 'symbols' + '?api_key=' + ApiKey)
+    response = requests.get(base_url + 'symbols?' + 'api_key=' + ApiKey)
     list_of_symbols = response.json()
     print(list_of_symbols)
     requests.get('http://forex.1forge.com/1.0.3/')
