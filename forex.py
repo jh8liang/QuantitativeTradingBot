6f3dfd3964208b474985f3b4c69c63f42ed3d349
ohshizournizz <jamiejiahao@gmail.com>
2019-01-08 22:01:54 -0500

    reteive request from http

--- forex.py
+++ forex.py
@@ -1,9 +1,13 @@
 import json
 import requests
 
+ApiKey = 'lPpKLU2BhzWU3eB2iWHeZ0XG5fO241JP'
 base_url = 'http://forex.1forge.com/1.0.3/'
 
 def main():
+    response = requests.get(base_url + 'symbols' + '?api_key=' + ApiKey)
+    list_of_symbols = response.json()
+    print(list_of_symbols)
     requests.get('http://forex.1forge.com/1.0.3/')
     print('hello')
 
